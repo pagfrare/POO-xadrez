e3a13e4de58b00c4a26dd5937bd9df589349202f
pagfrare <109170339+pagfrare@users.noreply.github.com>
2025-06-28 08:59:18 -0300

    Add arquivos base

---
+++ src/xadrez/Main.java
@@ -0,0 +1,12 @@
+package xadrez;
+/**
+ *
+ * @author G1 (Felipe(834688), Felipe(834732), Pedro(834765))
+ * 
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+    
+}
